1ac15bdd71d7dfe9921f59ca318fa14ad30217dc
Deso <Deso@DESKTOP-QCLKM4R>
2019-11-15 20:35:15 +0200

    GUI - early stage

---
+++ EventTicket/src/main/java/com/EventTicket/EventTicket/frame1.java
@@ -0,0 +1,103 @@
+package com.EventTicket.EventTicket;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JComboBox;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JToolBar;
+import javax.swing.JList;
+import javax.swing.AbstractListModel;
+import javax.swing.JTree;
+import javax.swing.tree.DefaultTreeModel;
+import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+import javax.swing.JPopupMenu;
+import java.awt.Component;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+public class frame1 {
+
+	private JFrame frmEt;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					frame1 window = new frame1();
+					window.frmEt.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public frame1() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frmEt = new JFrame();
+		frmEt.setTitle("ET");
+		frmEt.setBounds(100, 100, 850, 350);
+		frmEt.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmEt.getContentPane().setLayout(null);
+		
+		JLabel lblEventTicket = new JLabel("Event Ticket");
+		lblEventTicket.setBounds(10, 11, 59, 14);
+		frmEt.getContentPane().add(lblEventTicket);
+		
+		JButton btnLogin = new JButton("Login");
+		btnLogin.setBounds(735, 11, 89, 23);
+		frmEt.getContentPane().add(btnLogin);
+		
+		JButton btnOptions = new JButton("Options");
+		btnOptions.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+			}
+		});
+		btnOptions.setBounds(735, 48, 89, 23);
+		frmEt.getContentPane().add(btnOptions);
+		btnLogin.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				login log = new login();
+				log.loginFrame();
+				
+			}
+		});
+	}
+	private static void addPopup(Component component, final JPopupMenu popup) {
+		component.addMouseListener(new MouseAdapter() {
+			public void mousePressed(MouseEvent e) {
+				if (e.isPopupTrigger()) {
+					showMenu(e);
+				}
+			}
+			public void mouseReleased(MouseEvent e) {
+				if (e.isPopupTrigger()) {
+					showMenu(e);
+				}
+			}
+			private void showMenu(MouseEvent e) {
+				popup.show(e.getComponent(), e.getX(), e.getY());
+			}
+		});
+	}
+}
---
+++ EventTicket/src/main/java/com/EventTicket/EventTicket/login.java
@@ -0,0 +1,98 @@
+package com.EventTicket.EventTicket;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
+import javax.swing.JFormattedTextField;
+import javax.swing.JPasswordField;
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class login extends JFrame {
+
+	private JPanel contentPane;
+	private JPasswordField passwordField;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void loginFrame() {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					login frame = new login();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public login() {
+		setTitle("login");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 215);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		final JComboBox comboBox = new JComboBox();
+		comboBox.setModel(new DefaultComboBoxModel(new String[] {"admin", "organisator", "distributor"}));
+		comboBox.setBounds(149, 95, 105, 20);
+		contentPane.add(comboBox);
+		
+		JLabel lblUsername = new JLabel("Username");
+		lblUsername.setBounds(43, 42, 60, 14);
+		contentPane.add(lblUsername);
+		
+		JLabel lblPassword = new JLabel("Password");
+		lblPassword.setBounds(43, 67, 60, 14);
+		contentPane.add(lblPassword);
+		
+		JFormattedTextField formattedTextField = new JFormattedTextField();
+		formattedTextField.setBounds(149, 39, 218, 20);
+		contentPane.add(formattedTextField);
+		
+		passwordField = new JPasswordField();
+		passwordField.setBounds(149, 64, 218, 20);
+		contentPane.add(passwordField);
+		
+		JButton btnLogin = new JButton("Login");
+		btnLogin.addActionListener(new ActionListener() {
+			@SuppressWarnings("static-access")
+			public void actionPerformed(ActionEvent e) {
+				TestMain obj = new TestMain();
+				if(String.valueOf(comboBox.getSelectedItem())=="admin") {
+					if(obj.getAdmin(33).getUsername() == String.valueOf(formattedTextField.getText()))
+					{
+						if(obj.getAdmin(33).getPassword() == String.valueOf(passwordField.getPassword())) {
+						dispose();
+						}
+					}
+				}
+				else if(String.valueOf(comboBox.getSelectedItem())=="organisator") {
+					
+				}
+				else if(String.valueOf(comboBox.getSelectedItem())=="distributor") {
+					
+				}
+				
+			}
+		});
+		btnLogin.setBounds(149, 145, 89, 23);
+		contentPane.add(btnLogin);
+	}
+
+}
