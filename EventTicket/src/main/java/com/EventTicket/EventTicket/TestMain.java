640de24ec75a1dfc58a665850b93625be32150b0
Deso <Deso@DESKTOP-QCLKM4R>
2019-11-15 20:34:17 +0200

    GUI- early stage

--- EventTicket/src/main/java/com/EventTicket/EventTicket/TestMain.java
+++ EventTicket/src/main/java/com/EventTicket/EventTicket/TestMain.java
@@ -1,16 +1,23 @@
 package com.EventTicket.EventTicket;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
+import javax.persistence.NoResultException;
 import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
 
 public class TestMain {
 
 private static	EntityManagerFactory ENTITY_MANAGER_FACTORY = Persistence.createEntityManagerFactory("EventTicket");
 	public static void main(String[] args) {
-		addAdmin(1,"test","test");
+		//addAdmin(1,"test","test");
+		//addAdmin(33,"admin2","admin2");
 		ENTITY_MANAGER_FACTORY.close();
+		frame1 mainframe = new frame1();
+		mainframe.main(args);
 
 	}
 	public static void addAdmin(int id,String username,String password)
@@ -100,6 +107,59 @@ private static	EntityManagerFactory ENTITY_MANAGER_FACTORY = Persistence.createE
 		{
 			em.close();
 			
+		}
+	}
+	public static Admin getAdmin(int id)
+	{
+		EntityManager em=ENTITY_MANAGER_FACTORY.createEntityManager();
+		String query = "SELECT admin FROM Admin admin WHERE admin.id = :idAdmin";
+		TypedQuery<Admin> tq = em.createQuery(query, Admin.class);
+		tq.setParameter("idADmin", id);
+		Admin adm = null;
+		try {
+		adm = tq.getSingleResult();
+		System.out.println(adm.getUsername());
+		}
+		catch(NoResultException ex) {
+			ex.printStackTrace();
+		}
+		//finally {
+			//em.close();
+		//}
+		return adm;
+	}
+	public static void getAdmins()
+	{
+		EntityManager em=ENTITY_MANAGER_FACTORY.createEntityManager();
+		String stringQuery = "SELECT admin FROM Admin admin WHERE idAdmin IS NOT NULL";
+		TypedQuery<Admin> tq = em.createQuery(stringQuery, Admin.class);
+		List<Admin> admins;
+		try {
+			admins = tq.getResultList();
+			admins.forEach(adm->System.out.println(adm.getUsername()+ " " + adm.getPassword()));
+		}
+		catch(NoResultException ex) {
+			ex.printStackTrace();
+		}
+		finally {
+			em.close();
+		}
+	}
+	public static void getAdminsByName()
+	{
+		EntityManager em=ENTITY_MANAGER_FACTORY.createEntityManager();
+		String stringQuery = "SELECT admin FROM Admin admin WHERE idAdmin IS NOT NULL";
+		TypedQuery<Admin> tq = em.createQuery(stringQuery, Admin.class);
+		List<Admin> admins;
+		try {
+			admins = tq.getResultList();
+			admins.forEach(adm->System.out.println(adm.getUsername()+ " " + adm.getPassword()));
+		}
+		catch(NoResultException ex) {
+			ex.printStackTrace();
+		}
+		finally {
+			em.close();
 		}
 	}
 }
